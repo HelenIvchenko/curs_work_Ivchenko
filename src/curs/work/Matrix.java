d47e3c58c95e430e0942c0cdd901d637f5f7dd8d
Olena_Ivchenko <helen.ivchenko@gmail.com>
2020-12-13 00:17:03 +0200

    add multiply and divide on number

--- src/curs/work/Matrix.java
+++ src/curs/work/Matrix.java
@@ -1,5 +1,86 @@
 package curs.work;
 
-public class Matrix {
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
+public class Matrix {
+    Scanner in = new Scanner(System.in);
+    int rows = -1;
+    int columns = -1;
+    String name;
+    static int counter = 0;
+    double[][] matrix;
+
+    Matrix() {
+        matrix = new double[0][0];
+    }
+
+    Matrix(String name) {
+        this.name = name;
+        while (rows <= 0) {
+            System.out.print("Введіть кількість рядків матриці " + name + " (ціле число більше 0): ");
+            int oldRows = rows;
+            try {
+                rows = in.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.print("Сталася помилка при спробі вводу");
+            }
+            if (rows == oldRows) break;
+        }
+
+        while (rows > 0 && columns <= 0) {
+            System.out.print("Введіть кількість колонок матриці " + name + " (ціле число більше 0): ");
+            int oldColumns = columns;
+            try {
+                columns = in.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.print("Сталася помилка при спробі вводу");
+            }
+            if (columns == oldColumns) break;
+        }
+
+        matrix = new double[rows][columns];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print("Введіть елемент a[" + (i + 1) + "]" + "[" + (j + 1) + "]" + " матриці  " + name + ": ");
+                try {
+                    matrix[i][j] = in.nextDouble();
+                } catch (InputMismatchException e) {
+                    System.out.print("Сталася помилка при спробі вводу");
+                }
+                //System.out.print(matrix[i][j] + " ");
+            }
+            //System.out.println();
+        }
+        System.out.print("Ваша матриця " + name + ":\n");
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+        counter++;
+    }
+
+    void multiplyOnNUmber(double number) {
+        System.out.printf("Результат множення матриці %s на число %.2f\n", name, number);
+        for (int i = 0; i < this.matrix.length; i++) {
+            for (int j = 0; j < this.matrix[i].length; j++) {
+                matrix[i][j] = matrix[i][j] * number;
+                System.out.print(this.matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    void devideOnNUmber(double number) {
+        System.out.printf("Результат ділення матриці %s на число %.2f\n", name, number);
+        for (int i = 0; i < this.matrix.length; i++) {
+            for (int j = 0; j < this.matrix[i].length; j++) {
+                matrix[i][j] = matrix[i][j] / number;
+                System.out.print(this.matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
--- src/curs/work/Matrix_app.java
+++ src/curs/work/Matrix_app.java
@@ -10,34 +10,96 @@ import java.util.Scanner;
 public class Matrix_app {
 
     public static void main(String args[]) {
+
+        Matrix matrixA = new Matrix();
+        Matrix matrixB = new Matrix();
+        int command = -1;
         Scanner in = new Scanner(System.in);
-        int rows1 = -1;
-        int columns1 = -1;
 
-        while (rows1 <= 0) {
-            System.out.print("Введіть кількість рядків матриці А (ціле число більше 0): ");
-            int oldRows = rows1;
-            try {
-                rows1 = in.nextInt();
-            } catch (InputMismatchException e) {
-                System.out.print("Сталася помилка при спробі вводу");
+        while (command != 0) {
+            System.out.print("1 - ввести матрицю, 2 - множення матриці на число, 3 - піднесення матриці до степеня," +
+                    " 4 - складання двох матриць, 5 - множення матриць, 6 - ділення матриці на число, 7 - пошук зворотньої матриці, 0 - вихід:   ");
+            command = in.nextInt();
+            switch (command) {
+                case (1): {
+                    if (Matrix.counter % 2 == 0) {
+                        matrixA = new Matrix("A");
+                        break;
+                    }
+                    if (Matrix.counter % 2 != 0) {
+                        matrixB = new Matrix("B");
+                        break;
+                    }
+                }
+                case (2): {
+                    int name;
+                    double n;
+                    System.out.println("1 - обрати матрицю A, 2 - обрати матрицю В");
+                    name = in.nextInt();
+                    System.out.println("Введіть множник:");
+                    n = in.nextDouble();
+                    switch (name) {
+                        case (1): {
+                            matrixA.multiplyOnNUmber(n);
+                            break;
+                        }
+                        case (2): {
+                            matrixB.multiplyOnNUmber(n);
+                            break;
+                        }
+                    }
+                }
+
+                case (6): {
+                    int name;
+                    double n;
+                    System.out.println("1 - обрати матрицю A, 2 - обрати матрицю В");
+                    name = in.nextInt();
+                    System.out.println("Введіть дільник:");
+                    n = in.nextDouble();
+                    switch (name) {
+                        case (1): {
+                            matrixA.devideOnNUmber(n);
+                            break;
+                        }
+                        case (2): {
+                            matrixB.devideOnNUmber(n);
+                            break;
+                        }
+                    }
+
+                }
             }
-            if (rows1 == oldRows) break;
+            //     Matrix matrixA = new Matrix("A");
+//        Scanner in = new Scanner(System.in);
+//        int rows1 = -1;
+//        int columns1 = -1;
+//
+//        while (rows1 <= 0) {
+//            System.out.print("Введіть кількість рядків матриці А (ціле число більше 0): ");
+//            int oldRows = rows1;
+//            try {
+//                rows1 = in.nextInt();
+//            } catch (InputMismatchException e) {
+//                System.out.print("Сталася помилка при спробі вводу");
+//            }
+//            if (rows1 == oldRows) break;
+//        }
+//
+//        while (rows1 > 0 && columns1 <= 0) {
+//            System.out.print("Введіть кількість колонок матриці А (ціле число більше 0): ");
+//            int oldColumns = columns1;
+//            try {
+//                columns1 = in.nextInt();
+//            } catch (InputMismatchException e) {
+//                System.out.print("Сталася помилка при спробі вводу");
+//            }
+//            if (columns1 == oldColumns) break;
+//        }
+
+
+            //    System.out.println(matrixA.rows + " " + matrixA.columns);
         }
 
-        while (rows1 > 0 && columns1 <= 0) {
-            System.out.print("Введіть кількість колонок матриці А (ціле число більше 0): ");
-            int oldColumns = columns1;
-            try {
-                columns1 = in.nextInt();
-            } catch (InputMismatchException e) {
-                System.out.print("Сталася помилка при спробі вводу");
-            }
-            if (columns1 == oldColumns) break;
-        }
-
-
-        System.out.println(rows1 + " " + columns1);
     }
-
 }
